8911f84d898d4c1d56ba2c13db2d219280a3ac6e
ETIENNE <ETIENNE@DESKTOP-3>
2021-11-08 17:11:40 +0100

    hello

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : NetworkBehaviour
 {
@@ -16,6 +17,14 @@ public class Player : NetworkBehaviour
             float moveVertical = Input.GetAxis("Vertical");
             Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
             transform.position = transform.position + movement;
+        }
+    }
+
+    void OnBtnClick()
+    {
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.C))
+        {
+            SceneManager.LoadScene("Chat_Scene");
         }
     }
 
